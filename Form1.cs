e1ff119d5e8f949653d014780eb9559d477188b6
Robert Polomsky <robert.polomsky@autodesk.com>
2018-08-16 11:33:11 +0200

    Add other events

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,21 +13,49 @@ namespace PopUp
 {
     public partial class Form1 : Form
     {
-        private int _lastWarnedHour = -1;
+        private readonly List<PopEvent> _popEvents = new List<PopEvent>();
         public Form1()
         {
+            for (int i = 8; i <= 12; i++)
+                _popEvents.Add(new PopEvent(null, i, 0, "Wilds Boss"));
+
+            for (int i = 13; i <= 20; i++)
+                _popEvents.Add(new PopEvent(null, i, 0, "Wilds Boss, Boss' Lair"));
+
+            for (int i = 21; i <= 26; i++)
+                _popEvents.Add(new PopEvent(null, i, 0, "Wilds Boss"));
+
+            _popEvents.Add(new PopEvent(null, 12, 30, "3V3"));
+            _popEvents.Add(new PopEvent(null, 20, 30, "3V3"));
+
+            _popEvents.Add(new PopEvent(null, 13, 30, "Zombie Crisis"));
+            _popEvents.Add(new PopEvent(null, 14, 30, "Zombie Crisis"));
+            _popEvents.Add(new PopEvent(null, 17, 30, "Zombie Crisis"));
+            _popEvents.Add(new PopEvent(null, 18, 30, "Zombie Crisis"));
+            _popEvents.Add(new PopEvent(null, 19, 30, "Zombie Crisis"));
+            _popEvents.Add(new PopEvent(null, 20, 30, "Zombie Crisis"));
+
+            _popEvents.Add(new PopEvent(null, 19, 30, "Empire Strike"));
+
+            _popEvents.Add(new PopEvent(DayOfWeek.Wednesday, 21, 00, "Alliance War"));
+
+            _popEvents.Add(new PopEvent(DayOfWeek.Friday, 21, 00, "Holy Grail War"));
+
             InitializeComponent();
             WindowState = FormWindowState.Minimized;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (DateTime.Now.Minute == 56 && DateTime.Now.Hour != _lastWarnedHour)
+            foreach (PopEvent popEvent in _popEvents)
             {
-                WindowState = FormWindowState.Minimized;
-                Show();
-                WindowState = FormWindowState.Normal;
-                _lastWarnedHour = DateTime.Now.Hour;
+                if (popEvent.IsTime())
+                {
+                    WindowState = FormWindowState.Minimized;
+                    Show();
+                    WindowState = FormWindowState.Normal;
+                    Thread.Sleep(TimeSpan.FromMinutes(1));
+                }
             }
         }
     }
---
+++ PopEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PopUp
+{
+    class PopEvent
+    {
+        private string _description;
+        private readonly DayOfWeek? _day;
+        private readonly int _hour;
+        private readonly int _minute;
+
+        public PopEvent(DayOfWeek? day, int hour, int minute, string description)
+        {
+            _day = day;
+            _hour = hour - 1;
+            if (_hour < 0) _hour += 24;
+            _minute = minute - 3; // const add -3 minutes before event
+            if (_minute < 0) _minute += 60;
+            _description = description;
+        }
+
+        public bool IsTime()
+        {
+            if (_day != null && _day != DateTime.Now.DayOfWeek) return false;
+            return DateTime.Now.Hour == _hour && DateTime.Now.Minute == _minute;
+        }
+    }
+}
